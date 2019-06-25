c4b397d2b76d069d8ed23604f6e192d918d63322
Smileyezzz <david40217.lch@gmail.com>
2019-06-26 02:45:26 +0800

    Add the icon for spawning

--- Assets/Script/Spawn.cs
+++ Assets/Script/Spawn.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-    public GameObject Cat;
-    // Start is called before the first frame update
-    void Start()
+    public GameObject Cat_Normal;
+    public GameObject Cat_Axe;
+    public GameObject Cat_HipHop;
+    public GameObject Cat_Groose;
+    public GameObject Tower;
+
+    public void Produce_Normal()
     {
-        
+        Vector3 pos = Tower.transform.position + new Vector3(-5f, -2.2f, 0);
+        Instantiate(Cat_Normal, pos, Tower.transform.rotation);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Produce_Axe()
     {
-        if(Input.GetKeyDown(KeyCode.L))
-        {
-            Vector3 pos = gameObject.transform.position + new Vector3(-5f, -1.8f, 0);
-            Instantiate(Cat, pos, gameObject.transform.rotation);
-        }
+        Vector3 pos = Tower.transform.position + new Vector3(-5f, -2.2f, 0);
+        Instantiate(Cat_Axe, pos, Tower.transform.rotation);
+    }
+
+    public void Produce_HipHop()
+    {
+        Vector3 pos = Tower.transform.position + new Vector3(-5f, -2.2f, 0);
+        Instantiate(Cat_HipHop, pos, Tower.transform.rotation);
+    }
+
+    public void Produce_Groose()
+    {
+        Vector3 pos = Tower.transform.position + new Vector3(-5f, -2.2f, 0);
+        Instantiate(Cat_Groose, pos, Tower.transform.rotation);
     }
 }
