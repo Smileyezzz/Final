ac575ac800dce1e87f4612126c4438ae820f3f4f
Smileyezzz <david40217.lch@gmail.com>
2019-06-26 19:06:05 +0800

    HGHG

---
+++ Assets/Script/Botton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Botton : MonoBehaviour
+{
+    public Button Button1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button1.onClick.AddListener(Button1Clicked);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
